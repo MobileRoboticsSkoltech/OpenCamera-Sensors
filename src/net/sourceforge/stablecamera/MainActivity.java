0c7d5bb14c7ebaeaa647b74d9cbff1e9ca65f237
Mark Harman <mark.harman@ntlworld.com>
2013-09-06 00:06:33 +0100

    Save in StableCamera/ rather than MyCamera/

--- src/net/sourceforge/stablecamera/MainActivity.java
+++ src/net/sourceforge/stablecamera/MainActivity.java
@@ -89,7 +89,7 @@ public class MainActivity extends Activity {
         // To be safe, you should check that the SDCard is mounted
         // using Environment.getExternalStorageState() before doing this.
 
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "MyCamera");
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "StableCamera");
         // This location works best if you want the created images to be shared
         // between applications and persist after your app has been uninstalled.
 
