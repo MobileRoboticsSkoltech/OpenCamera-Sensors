546fd13d5471d6cd5a042707c2d03d0b66f2b2f5
Mark Harman <mark.harman@ntlworld.com>
2015-01-17 17:57:53 +0000

    UPDATED Now uses Material Design theme style on Android 5.

--- gen/net/sourceforge/opencamera/R.java
+++ gen/net/sourceforge/opencamera/R.java
@@ -385,6 +385,12 @@ public final class R {
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
+
+        Base application theme for API 21+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 21+ devices.
+    
+ API 21 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
@@ -397,6 +403,7 @@ public final class R {
         
  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
+ API 21 theme customizations can go here. 
          */
         public static final int PreferenceBaseTheme=0x7f090001;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
