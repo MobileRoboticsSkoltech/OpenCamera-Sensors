2b75a3149142739d2e4c0e3cb752e64f017a4887
Mark Harman <mark.harman.dev@gmail.com>
2017-05-07 20:22:48 +0100

    Fix Android warnings.

--- app/src/main/java/net/sourceforge/opencamera/ImageSaver.java
+++ app/src/main/java/net/sourceforge/opencamera/ImageSaver.java
@@ -639,7 +639,13 @@ public class ImageSaver extends Thread {
 			if( MyDebug.LOG )
 				Log.d(TAG, "before HDR first bitmap: " + bitmaps.get(0) + " is mutable? " + bitmaps.get(0).isMutable());
 			try {
-				hdrProcessor.processHDR(bitmaps, true, null, true, null, 0.5f, HDRProcessor.TonemappingAlgorithm.TONEMAPALGORITHM_REINHARD); // this will recycle all the bitmaps except bitmaps.get(0), which will contain the hdr image
+				if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ) {
+					hdrProcessor.processHDR(bitmaps, true, null, true, null, 0.5f, HDRProcessor.TonemappingAlgorithm.TONEMAPALGORITHM_REINHARD); // this will recycle all the bitmaps except bitmaps.get(0), which will contain the hdr image
+				}
+				else {
+					Log.e(TAG, "shouldn't have offered HDR as an option if not on Android 5");
+					throw new RuntimeException()
+				}
 			}
 			catch(HDRProcessorException e) {
 				Log.e(TAG, "HDRProcessorException from processHDR: " + e.getCode());
