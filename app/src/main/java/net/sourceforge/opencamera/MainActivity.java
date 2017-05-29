72c358995534fbfde02719cc1ba5cefe70d335a8
Mark Harman <mark.harman.dev@gmail.com>
2017-05-29 15:54:57 +0100

    Catch SecurityException from takePersistableUriPermission().

--- app/src/main/java/net/sourceforge/opencamera/MainActivity.java
+++ app/src/main/java/net/sourceforge/opencamera/MainActivity.java
@@ -1694,20 +1694,40 @@ public class MainActivity extends Activity implements AudioListener.AudioListene
 	    		final int takeFlags = resultData.getFlags()
 	    	            & (Intent.FLAG_GRANT_READ_URI_PERMISSION
 	    	            | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-		    	// Check for the freshest data.
-		    	getContentResolver().takePersistableUriPermission(treeUri, takeFlags);
-				SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-				SharedPreferences.Editor editor = sharedPreferences.edit();
-				editor.putString(PreferenceKeys.getSaveLocationSAFPreferenceKey(), treeUri.toString());
-				editor.apply();
-	
-				if( MyDebug.LOG )
-					Log.d(TAG, "update folder history for saf");
-				updateFolderHistorySAF(treeUri.toString());
+				try {
+					/*if( true )
+						throw new SecurityException(); // test*/
+					// Check for the freshest data.
+					getContentResolver().takePersistableUriPermission(treeUri, takeFlags);
 
-				File file = applicationInterface.getStorageUtils().getImageFolder();
-				if( file != null ) {
-					preview.showToast(null, getResources().getString(R.string.changed_save_location) + "\n" + file.getAbsolutePath());
+					SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+					SharedPreferences.Editor editor = sharedPreferences.edit();
+					editor.putString(PreferenceKeys.getSaveLocationSAFPreferenceKey(), treeUri.toString());
+					editor.apply();
+
+					if( MyDebug.LOG )
+						Log.d(TAG, "update folder history for saf");
+					updateFolderHistorySAF(treeUri.toString());
+
+					File file = applicationInterface.getStorageUtils().getImageFolder();
+					if( file != null ) {
+						preview.showToast(null, getResources().getString(R.string.changed_save_location) + "\n" + file.getAbsolutePath());
+					}
+				}
+				catch(SecurityException e) {
+					Log.e(TAG, "SecurityException failed to take permission");
+					e.printStackTrace();
+					// failed - if the user had yet to set a save location, make sure we switch SAF back off
+					SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+					String uri = sharedPreferences.getString(PreferenceKeys.getSaveLocationSAFPreferenceKey(), "");
+					if( uri.length() == 0 ) {
+						if( MyDebug.LOG )
+							Log.d(TAG, "no SAF save location was set");
+						SharedPreferences.Editor editor = sharedPreferences.edit();
+						editor.putBoolean(PreferenceKeys.getUsingSAFPreferenceKey(), false);
+						editor.apply();
+						preview.showToast(null, R.string.saf_permission_failed);
+					}
 				}
 	        }
 	        else {
