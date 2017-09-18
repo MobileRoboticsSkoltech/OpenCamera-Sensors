d6df2e5e930521fcbb8b32589f40a5a45101b4f4
Mark Harman <mark.harman.dev@gmail.com>
2017-09-18 23:18:52 +0100

    Run new testHDR41, testHDR42.

--- app/src/androidTest/java/net/sourceforge/opencamera/test/HDRTests.java
+++ app/src/androidTest/java/net/sourceforge/opencamera/test/HDRTests.java
@@ -55,7 +55,8 @@ public class HDRTests {
 		suite.addTest(TestSuite.createTest(MainActivityTest.class, "testHDR40"));
 		suite.addTest(TestSuite.createTest(MainActivityTest.class, "testHDR40Exponential"));
 		suite.addTest(TestSuite.createTest(MainActivityTest.class, "testHDR40Filmic"));
-		// don't run testHDR41 or testHDR42 yet
+		suite.addTest(TestSuite.createTest(MainActivityTest.class, "testHDR41"));
+		suite.addTest(TestSuite.createTest(MainActivityTest.class, "testHDR42"));
         return suite;
     }
 }
