63714b10ecebb9847016d31048b94b1674f9f008
Mark Harman <mark.harman.apps@gmail.com>
2017-12-01 23:44:30 +0000

    Workaround for Google bug where crash can be reported if application is open when it's being updated.

---
+++ app/src/main/java/net/sourceforge/opencamera/OpenCameraApplication.java
@@ -0,0 +1,30 @@
+package net.sourceforge.opencamera;
+
+import android.app.Application;
+import android.os.Process;
+import android.util.Log;
+
+/** We override the Application class to implement the workaround at
+ *  https://issuetracker.google.com/issues/36972466#comment14 for Google bug crash. It seems ugly,
+ *  but Google consider this a low priority despite calling these "bad behaviours" in applications!
+ */
+public class OpenCameraApplication extends Application {
+	private static final String TAG = "OpenCameraApplication";
+
+    @Override
+    public void onCreate() {
+		if( MyDebug.LOG )
+			Log.d(TAG, "onCreate");
+        super.onCreate();
+        checkAppReplacingState();
+    }
+
+    private void checkAppReplacingState() {
+		if( MyDebug.LOG )
+			Log.d(TAG, "checkAppReplacingState");
+        if( getResources() == null ) {
+            Log.e(TAG, "app is replacing, kill");
+            Process.killProcess(Process.myPid());
+        }
+    }
+}
