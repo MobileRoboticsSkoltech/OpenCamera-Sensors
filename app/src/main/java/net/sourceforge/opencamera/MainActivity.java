94e6dc5634524fd4776a5c31928b30ce8ead6c35
Mark Harman <mark.harman.apps@gmail.com>
2018-05-19 16:05:23 +0100

    Require Android 7 for NR mode; don't rely on auto stabilise flag for HDR support check.

--- app/src/main/java/net/sourceforge/opencamera/MainActivity.java
+++ app/src/main/java/net/sourceforge/opencamera/MainActivity.java
@@ -3071,10 +3071,9 @@ public class MainActivity extends Activity implements AudioListener.AudioListene
 	}
 
     public boolean supportsHDR() {
-    	// we also require the device have sufficient memory to do the processing, simplest to use the same test as we do for auto-stabilise...
-		// (note we don't call supportsAutoStabilise(), as that does other checks specifically for auto-stabilise, e.g., calling applicationInterface.isRawOnly() - which would get an infinite loop if called from here!)
+    	// we also require the device have sufficient memory to do the processing
 		// also require at least Android 5, for the Renderscript support in HDRProcessor
-		return( Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && this.supports_auto_stabilise && preview.supportsExpoBracketing() );
+		return( Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && large_heap_memory >= 128 && preview.supportsExpoBracketing() );
     }
     
     public boolean supportsExpoBracketing() {
@@ -3088,7 +3087,9 @@ public class MainActivity extends Activity implements AudioListener.AudioListene
 	}
 
     public boolean supportsNoiseReduction() {
-		return( Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && preview.usingCamera2API() && large_heap_memory >= 512 && preview.supportsBurst() && preview.supportsExposureTime() );
+		// require at least Android 5, for the Renderscript support in HDRProcessor, but we require
+		// Android 7 to limit to more modern devices (for performance reasons)
+		return( Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && preview.usingCamera2API() && large_heap_memory >= 512 && preview.supportsBurst() && preview.supportsExposureTime() );
 		//return false; // currently blocked for release
 	}
     
