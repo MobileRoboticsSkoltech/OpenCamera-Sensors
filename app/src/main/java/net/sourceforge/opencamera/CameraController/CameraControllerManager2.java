7bc9422455fc37b94f88dd78e1d933e0900ee5ea
Mark Harman <mark.harman.apps@gmail.com>
2019-01-17 22:58:27 +0000

    Fix comment.

--- app/src/main/java/net/sourceforge/opencamera/CameraController/CameraControllerManager2.java
+++ app/src/main/java/net/sourceforge/opencamera/CameraController/CameraControllerManager2.java
@@ -94,9 +94,8 @@ public class CameraControllerManager2 extends CameraControllerManager {
 		return requiredLevel <= deviceLevel;
 	}
 
-	/* Rather than allowing Camera2 API on all Android 5+ devices, we restrict it to cases where all cameras have at least LIMITED support.
-	 * (E.g., Nexus 6 has FULL support on back camera, LIMITED support on front camera.)
-	 * For now, devices with only LEGACY support should still use the old API.
+	/* Rather than allowing Camera2 API on all Android 5+ devices, we restrict it to certain cases.
+	 * This returns whether the specified camera has at least LIMITED support.
 	 */
 	public boolean allowCamera2Support(int cameraId) {
 		CameraManager manager = (CameraManager)context.getSystemService(Context.CAMERA_SERVICE);
