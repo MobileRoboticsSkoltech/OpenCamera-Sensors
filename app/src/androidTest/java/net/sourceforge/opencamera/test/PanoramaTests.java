2243f7dddad6b9d7545756dd89ab1ae8d474657d
Mark Harman <mark.harman.apps@gmail.com>
2019-02-10 22:22:28 +0000

    Add panorama test suite.

---
+++ app/src/androidTest/java/net/sourceforge/opencamera/test/PanoramaTests.java
@@ -0,0 +1,19 @@
+package net.sourceforge.opencamera.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class PanoramaTests {
+    /** Tests for Panorama algorithm - only need to run on a single device
+     *  Should manually look over the images dumped onto DCIM/
+     *  To use these tests, the testdata/ subfolder should be manually copied to the test device in the DCIM/testOpenCamera/
+     *  folder (so you have DCIM/testOpenCamera/testdata/). We don't use assets/ as we'd end up with huge APK sizes which takes
+     *  time to transfer to the device every time we run the tests.
+     */
+    public static Test suite() {
+        TestSuite suite = new TestSuite(MainTests.class.getName());
+        suite.addTest(TestSuite.createTest(MainActivityTest.class, "testPanorama1"));
+        suite.addTest(TestSuite.createTest(MainActivityTest.class, "testPanorama2"));
+        return suite;
+    }
+}
