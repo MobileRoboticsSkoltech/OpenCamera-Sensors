27a39ef5ff4434aa5b48d01d26ce06a569f8dc14
Azat Akhmetyanov <a.r.akhmetyanov@gmail.com>
2020-11-24 19:18:57 +0300

    Change to physical device configuration from device list

--- app/src/androidTest/java/net/sourceforge/opencamera/test/SubsetTests.java
+++ app/src/androidTest/java/net/sourceforge/opencamera/test/SubsetTests.java
@@ -1,8 +1,11 @@
 package net.sourceforge.opencamera.test;
 
+import android.test.suitebuilder.annotation.SmallTest;
+
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+@SmallTest
 public class SubsetTests {
     public static Test suite() {
         TestSuite suite = new TestSuite(MainTests.class.getName());
