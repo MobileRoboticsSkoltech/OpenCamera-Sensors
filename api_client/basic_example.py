7c9cacab3287d18e8b263fdb05cce9bd3ca00c69
Marselka <ma.faizullin@gmail.com>
2020-12-15 14:24:28 +0300

    accel -> gyro in basic_example.py

--- api_client/basic_example.py
+++ api_client/basic_example.py
@@ -1,7 +1,7 @@
 import time
 from src.RemoteControl import RemoteControl
 
-HOST = '192.168.1.100'  # The smartphone's IP address
+HOST = '10.30.65.125'  # The smartphone's IP address
 
 
 def main():
@@ -10,10 +10,10 @@ def main():
     remote = RemoteControl(HOST)
 
     # remote.get_imu blocks until you get result (see async_imu_example for asynchronous usage in python 2)
-    accel_data, gyro_data = remote.get_imu(10000, want_accel=True, want_gyro=False)
-    print("Accelerometer data length: %d" % len(accel_data))
-    with open("accel.csv", "w+") as accel:
-        accel.writelines(accel_data)
+    accel_data, gyro_data = remote.get_imu(10000, want_accel=False, want_gyro=True)
+    print("Gyroscope data length: %d" % len(gyro_data))
+    with open("gyro.csv", "w+") as gyro:
+        gyro.writelines(gyro_data)
 
     # starts video and returns phase and duration in nanoseconds
     phase_ns, duration_ns = remote.start_video()
