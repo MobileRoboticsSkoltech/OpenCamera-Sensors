77fa838fe48a9bcc6f0a0f02c8c47b779c5a4ed3
Marselka <ma.faizullin@gmail.com>
2020-12-15 14:44:43 +0300

    accel -> gyro in async_imu_example.py

--- api_client/async_imu_example.py
+++ api_client/async_imu_example.py
@@ -2,24 +2,24 @@ import time
 from src.RemoteControl import RemoteControl
 from concurrent.futures import ThreadPoolExecutor
 
-HOST = '192.168.1.100'  # The smartphone's IP address
+HOST = '10.30.65.125'  # The smartphone's IP address
 
 
 def main():
     remote = RemoteControl(HOST)
 
     with ThreadPoolExecutor(max_workers=1) as executor:
-        future = executor.submit(remote.get_imu, 10000, True, False)
+        future = executor.submit(remote.get_imu, 1000, False, True)
         # Do something else
         print("doing other stuff...")
-        time.sleep(10)
+        time.sleep(1)
         print("done doing other stuff")
         # Get result when needed
         accel_data, gyro_data = future.result()
         # Process result somehow (here just file output)
-        print("Accelerometer data length: %d" % len(accel_data))
-        with open("accel.csv", "w+") as accel:
-            accel.writelines(accel_data)
+        print("Gyroscope data length: %d" % len(gyro_data))
+        with open("gyro.csv", "w+") as gyro:
+            gyro.writelines(gyro_data)
 
     print('EXITED')
     remote.close()
